36d4764d18dcad716f79809e7235ddbc1b66a478
denizderK <denizderya8668@gmail.com>
2023-10-30 20:59:45 +0300

    fs

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 //http server oluşturuyoruz
 
 var http = require("http")  //node modüles => http, fs, os
+var fs =require("fs");
 
 var server =http.createServer((req,res) =>{
-    console.log(req.url)
 
-    res.end();
+    if(req.url=="/"){
+        res.write("<h1>anasayfa</h1> ")
+    }else if(req.url == "/urunler"){
+        res.write("<h1>ürünler</h1> ")
+    }else{
+        res.write("sayfa bulunamadı ")
+    }
+    res.end();  //cevap göndermezsek tarayıcı işlev göstermez sürekli yeniliyor modunda olur res=response cevap demek. servere durdurmuş oldun
 })
 
 server.listen(3000, ()=>{
